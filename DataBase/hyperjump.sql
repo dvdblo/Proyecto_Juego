b63e795c03fc806b6e96161469243b6c7aed7bfa
DanielArmas12 <A01786896@tec.mx>
2026-04-20 12:27:13 -0600

    End points para login y registros, ademas de la correción completa de la bse de datos

--- DataBase/hyperjump.sql
+++ DataBase/hyperjump.sql
@@ -2,142 +2,172 @@ DROP DATABASE IF EXISTS hyperjump;
 CREATE DATABASE hyperjump;
 USE hyperjump;
 
-CREATE TABLE Jugador(id_jugador INT AUTO_INCREMENT PRIMARY KEY, 
-	contraseña VARCHAR(255) NOT NULL, 
-    fecha_registro DATETIME NOT NULL);
+CREATE TABLE Jugador(id_jugador INT AUTO_INCREMENT,
+    username VARCHAR(50) UNIQUE NOT NULL,
+	contraseña VARCHAR(15) NOT NULL,
+    edad TINYINT UNSIGNED,
+    fecha_registro TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_jugador PRIMARY KEY(id_jugador)
+    ) ENGINE=InnoDB;
 
-CREATE TABLE Estadisticas(id_estadistica INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Estadisticas(id_estadistica INT AUTO_INCREMENT,
+    id_jugador INT NOT NULL UNIQUE,
+    enemigos_eliminados MEDIUMINT UNSIGNED DEFAULT 0,
+    cartas_usadas MEDIUMINT UNSIGNED DEFAULT 0,
+    cartas_mejoradas SMALLINT UNSIGNED DEFAULT 0,
+    victorias SMALLINT UNSIGNED DEFAULT 0,
+    derrotas SMALLINT UNSIGNED DEFAULT 0,
+    tiempo_jugado INT UNSIGNED DEFAULT 0,
+    nivel_maximo_alcanzado TINYINT UNSIGNED DEFAULT 0,
+    puntuacion_maxima INT UNSIGNED DEFAULT 0,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_estadisticas PRIMARY KEY(id_estadistica),
+    CONSTRAINT fk_estadisticas_jugador FOREIGN KEY (id_jugador) REFERENCES Jugador(id_jugador)
+) ENGINE=InnoDB;
+
+CREATE TABLE Partida(id_partida INT AUTO_INCREMENT,
     id_jugador INT NOT NULL,
-    enemigos_eliminados INT DEFAULT 0,
-    cartas_usadas INT DEFAULT 0,
-    cartas_mejoradas INT DEFAULT 0,
-    victorias INT DEFAULT 0,
-    derrotas INT DEFAULT 0,
-    tiempo_jugado INT DEFAULT 0,
-    nivel_maximo_alcanzado INT DEFAULT 0,
-    puntuacion_maxima INT DEFAULT 0,
-    FOREIGN KEY (id_jugador) REFERENCES Jugador(id_jugador)
-);
+    fecha_inicio TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    fecha_fin TIMESTAMP NULL,
+    tiempo_total_seg INT UNSIGNED DEFAULT 0,
+    puntaje_total INT UNSIGNED DEFAULT 0,
+    niveles_completados TINYINT UNSIGNED DEFAULT 0,
+    enemigos_total SMALLINT UNSIGNED DEFAULT 0,
+    vidas_restantes TINYINT UNSIGNED DEFAULT 3,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_partida PRIMARY KEY(id_partida),
+    CONSTRAINT fk_partida_jugador FOREIGN KEY (id_jugador) REFERENCES Jugador(id_jugador)
+) ENGINE=InnoDB;
 
-CREATE TABLE Partida(id_partida INT AUTO_INCREMENT PRIMARY KEY,
-    id_jugador INT NOT NULL,
-    fecha_inicio DATETIME NOT NULL,
-    fecha_fin DATETIME,
-    puntaje_total INT DEFAULT 0,
-    niveles_completados INT DEFAULT 0,
-    enemigos_total INT DEFAULT 0,
-    vidas_restantes INT DEFAULT 0,
-    FOREIGN KEY (id_jugador) REFERENCES Jugador(id_jugador)
-);
+CREATE TABLE Nivel(id_nivel INT AUTO_INCREMENT,
+    numero_nivel TINYINT UNSIGNED NOT NULL,
+    dificultad ENUM('facil', 'medio', 'dificil') NOT NULL,
+    tiempo_limite_seg SMALLINT UNSIGNED,
+    CONSTRAINT pk_nivel PRIMARY KEY(id_nivel)
+) ENGINE=InnoDB;
 
-CREATE TABLE Nivel(id_nivel INT AUTO_INCREMENT PRIMARY KEY,
-    dificultad INT NOT NULL,
-    numero_nivel INT NOT NULL,
-    tiempo_limite_seg INT,
-    tiene_jefe BOOLEAN,
-    tiene_jefe2 BOOLEAN
-);
+CREATE TABLE JefeNivel(id_jefe_nivel INT AUTO_INCREMENT,
+	id_nivel INT NOT NULL,
+    nombre_jefe VARCHAR(50),
+    CONSTRAINT pk_JefeNivel PRIMARY KEY(id_jefe_nivel),
+    CONSTRAINT fk_JefeNivel_nivel FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel)
+) ENGINE=InnoDB;
 
-CREATE TABLE NivelPartida(id_nivel_partida INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE NivelPartida(id_nivel_partida INT AUTO_INCREMENT,
     id_partida INT NOT NULL,
     id_nivel INT NOT NULL,
-    puntaje_nivel INT DEFAULT 0,
-    tiempo_segundos INT,
-    enemigos_eliminados INT DEFAULT 0,
-    cartas_usadas INT DEFAULT 0,
-    cartas_no_usadas INT DEFAULT 0,
-    multiplicador_tiempo DECIMAL(5,2),
-    completado BOOLEAN,
-    FOREIGN KEY (id_partida) REFERENCES Partida(id_partida),
-    FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel)
-);
+    puntaje_nivel INT UNSIGNED DEFAULT 0,
+    tiempo_segundos SMALLINT UNSIGNED DEFAULT 0,
+    enemigos_eliminados SMALLINT UNSIGNED DEFAULT 0,
+    cartas_usadas SMALLINT UNSIGNED DEFAULT 0,
+    cartas_no_usadas SMALLINT UNSIGNED DEFAULT 0,
+    multiplicador_tiempo DECIMAL(5,2) DEFAULT 1.00,
+    completado BOOLEAN DEFAULT FALSE,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_NivelPartida PRIMARY KEY(id_nivel_partida),
+    CONSTRAINT fk_NivelPartida_partida FOREIGN KEY (id_partida) REFERENCES Partida(id_partida),
+    CONSTRAINT fk_NivelPartida_nivel FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel)
+) ENGINE=InnoDB;
 
-CREATE TABLE Carta(id_carta INT AUTO_INCREMENT PRIMARY KEY,
-    descripcion VARCHAR(60),
-    nivel_maximo INT,
-    dificultad_requerida VARCHAR(40) UNIQUE
-);
+CREATE TABLE Carta(id_carta INT AUTO_INCREMENT,
+    descripcion VARCHAR(100),
+    nivel_maximo TINYINT UNSIGNED,
+    dificultad_requerida ENUM('facil', 'medio', 'dificil'),
+    CONSTRAINT pk_Carta PRIMARY KEY(id_carta)
+)ENGINE=InnoDB;
 
-CREATE TABLE CartaJugador(id_carta_jugador INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE CartaJugador(id_carta_jugador INT AUTO_INCREMENT,
     id_jugador INT NOT NULL,
     id_carta INT NOT NULL,
-    nivel_actual INT DEFAULT 1,
-    veces_mejorada INT DEFAULT 0,
-    veces_usada_total INT DEFAULT 0,
-    FOREIGN KEY (id_jugador) REFERENCES Jugador(id_jugador),
-    FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
-);
+    nivel_actual TINYINT UNSIGNED DEFAULT 1,
+    veces_mejorada SMALLINT UNSIGNED DEFAULT 0,
+    veces_usada_total MEDIUMINT UNSIGNED DEFAULT 0,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_CartaJugador PRIMARY KEY(id_carta_jugador),
+    CONSTRAINT fk_CartaJugador_jugador FOREIGN KEY (id_jugador) REFERENCES Jugador(id_jugador),
+    CONSTRAINT fk_CartaJugador_carta FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
+)ENGINE=InnoDB;
 
-CREATE TABLE CartaPartida(id_carta_partida INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE CartaPartida(id_carta_partida INT AUTO_INCREMENT,
     id_nivel_partida INT NOT NULL,
     id_carta INT NOT NULL,
-    nivel_carta_repartir INT,
-    fue_usada BOOLEAN,
-    fue_almacenada BOOLEAN,
-    veces_usada INT DEFAULT 0,
-    FOREIGN KEY (id_nivel_partida) REFERENCES NivelPartida(id_nivel_partida),
-    FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
-);
+    nivel_carta_repartir TINYINT UNSIGNED,
+    fue_usada BOOLEAN DEFAULT FALSE,
+    fue_almacenada BOOLEAN DEFAULT FALSE,
+    veces_usada SMALLINT UNSIGNED DEFAULT 0,
+    last_update TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    CONSTRAINT pk_CartaPartida PRIMARY KEY(id_carta_partida),
+    CONSTRAINT fk_CartaPartida_partida FOREIGN KEY (id_nivel_partida) REFERENCES NivelPartida(id_nivel_partida),
+    CONSTRAINT fk_CartaPartida_carta FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
+)ENGINE=InnoDB;
 
-CREATE TABLE Enemigo(id_enemigo INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Enemigo(id_enemigo INT AUTO_INCREMENT,
     nombre VARCHAR(60) UNIQUE,
-    tipo VARCHAR(60) UNIQUE,
-    vida_base INT,
-    daño_base INT,
-    es_inmortal BOOLEAN,
-    rango_ataque BOOLEAN,
-    rango_deteccion INT
-);
+    tipo VARCHAR(60),
+    descripcion VARCHAR(255),
+    vida_base SMALLINT UNSIGNED,
+    daño_base SMALLINT UNSIGNED,
+    es_inmortal BOOLEAN DEFAULT FALSE,
+    rango_ataque BOOLEAN DEFAULT FALSE, 
+    rango_deteccion SMALLINT UNSIGNED,
+    CONSTRAINT pk_enemigo PRIMARY KEY(id_enemigo)
+)ENGINE=InnoDB;
 
-CREATE TABLE EnemigoNivel(id_enemigo_nivel INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE EnemigoNivel(id_enemigo_nivel INT AUTO_INCREMENT,
     id_nivel INT NOT NULL,
     id_enemigo INT NOT NULL,
-    cantidad_maxima INT,
-    FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel),
-    FOREIGN KEY (id_enemigo) REFERENCES Enemigo(id_enemigo)
-);
+    cantidad_maxima SMALLINT UNSIGNED,
+    CONSTRAINT pk_EnemigoNivel PRIMARY KEY(id_enemigo_nivel),
+    CONSTRAINT fk_EnemigoNivel_nivel FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel),
+    CONSTRAINT fk_EnemigoNivel_enemigo FOREIGN KEY (id_enemigo) REFERENCES Enemigo(id_enemigo)
+)ENGINE=InnoDB;
 
-CREATE TABLE Plataforma(id_plataforma INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE Plataforma(id_plataforma INT AUTO_INCREMENT,
     id_carta INT,
     nombre VARCHAR(60),
     composicion JSON,
-    es_autogenerada BOOLEAN,
-    FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
-);
+    es_autogenerada BOOLEAN DEFAULT FALSE,
+    CONSTRAINT pk_Plataforma PRIMARY KEY(id_plataforma),
+    CONSTRAINT fk_Plataforma_carta FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
+)ENGINE=InnoDB;
 
-CREATE TABLE PlataformaNivel(id_plataforma_nivel INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE PlataformaNivel(id_plataforma_nivel INT AUTO_INCREMENT,
     id_plataforma INT NOT NULL,
-    nivel INT NOT NULL,
+    nivel_plataforma TINYINT UNSIGNED NOT NULL,
     ancho_base DECIMAL(10,2),
     alto_base DECIMAL(10,2),
-    duracion INT,
-    es_destruible BOOLEAN,
-    enemigo_encima BOOLEAN,
-    FOREIGN KEY (id_plataforma) REFERENCES Plataforma(id_plataforma)
-);
+    duracion SMALLINT UNSIGNED,
+    es_destruible BOOLEAN DEFAULT FALSE,
+    enemigo_encima BOOLEAN DEFAULT FALSE,
+    CONSTRAINT pk_PlataformaNivel PRIMARY KEY(id_plataforma_nivel),
+    CONSTRAINT fk_PlataformaNivel_plataforma FOREIGN KEY (id_plataforma) REFERENCES Plataforma(id_plataforma)
+)ENGINE=InnoDB;
 
-CREATE TABLE PowerUp(id_powerUp INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE PowerUp(id_powerUp INT AUTO_INCREMENT,
     id_carta INT NOT NULL,
     nombre VARCHAR(60),
-    FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
-);
+    CONSTRAINT pk_PowerUp PRIMARY KEY(id_powerUp),
+    CONSTRAINT fk_PowerUp_carta FOREIGN KEY (id_carta) REFERENCES Carta(id_carta)
+)ENGINE=InnoDB;
 
-CREATE TABLE PowerUpNivel(id_powerup_nivel INT AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE PowerUpNivel(id_powerup_nivel INT AUTO_INCREMENT,
     id_powerUp INT NOT NULL,
-    nivel INT NOT NULL,
-    duracion_base INT,
+    nivel_powerUp TINYINT UNSIGNED NOT NULL,
+    duracion_base SMALLINT UNSIGNED,
     modificador_velocidad DECIMAL(10,2),
     modificador_salto DECIMAL(10,2),
-    da_vida INT,
-    rango_actuacion INT,
-    FOREIGN KEY (id_powerUp) REFERENCES PowerUp(id_powerUp)
-);
+    da_vida TINYINT,
+    rango_actuacion SMALLINT UNSIGNED,
+    CONSTRAINT pk_PowerUpNivel PRIMARY KEY(id_powerup_nivel),
+    CONSTRAINT fk_PowerUpNivel_powerUp FOREIGN KEY (id_powerUp) REFERENCES PowerUp(id_powerUp)
+)ENGINE=InnoDB;
 
-CREATE TABLE ZonaGeneracion(id_zona INT AUTO_INCREMENT PRIMARY KEY,
-	id_nivel INT,
-    coord_x INT,
-    coord_y INT,
-    hostil BOOL,
-    FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel)
-    );
-
+CREATE TABLE ZonaGeneracion(id_zona INT AUTO_INCREMENT,
+	id_nivel INT NOT NULL,
+    coord_x SMALLINT,
+    coord_y SMALLINT,
+    hostil BOOL DEFAULT FALSE,
+    CONSTRAINT pk_ZonaGeneracion PRIMARY KEY(id_zona),
+    CONSTRAINT fk_ZonaGeneracion_nivel FOREIGN KEY (id_nivel) REFERENCES Nivel(id_nivel)
+)ENGINE=InnoDB;
